14d81745d2e29424d9f9c3ae896d46f64636fdd3
lucas rodriguez <lucasrodriguez805@gmail.com>
2017-12-20 23:02:07 +0000

    make the winner appear on site

--- js/script.js
+++ js/script.js
@@ -9,33 +9,39 @@ else if (computerChoice <= 0.67) {
 else {
     computerChoice = "scissors";
 }
-var compare = function(choice1, choice2) {
-    if (choice1 === choice2) {
-        return "The result is a tie!";
+
+$(document).ready(function() {
+    console.log("ready!");
+    $("#shoot").click(function() {
+        var userChoice = $("#choice").val()
+        var computerChoice = getComputerChoice()
+        $("#userChoice").html(userChoice);
+        $("#computerChoice").html(computerChoice);
+        chooseWinner(userChoice, computerChoice)
+    })
+});
+
+function getComputerChoice() {
+    var x = 10;
+    var options = ["rock", "paper", "scissors"];
+
+    var index = Math.floor(Math.random() * 3);
+    return options[index];
+}
+function chooseWinner(userChoice, computerChoice) {
+    if(userChoice==computerChoice){
+        console.log("It's A Tie")
+    } else if (userChoice=="rock"&&computerChoice=="paper"){
+        console.log("Computer Wins!")
+    } else if (userChoice=="rock"&&computerChoice=="scissors"){
+        console.log("User Wins!")
+    } else if (userChoice=="paper"&&computerChoice=="scissors"){
+        console.log("Computer Wins!")
+    } else if (userChoice=="paper"&&computerChoice=="rock"){
+        console.log("User Wins!")
+    } else if (userChoice=="scissors"&&computerChoice=="paper"){
+        console.log("User Wins!")
+    } else if (userChoice=="scissors"&&computerChoice=="rock"){
+        console.log("Computer Wins!")
     }
-    if (choice1 === "rock") {
-        if (choice2 === "scissors") {
-            return "rock wins";
-        }
-        else {
-            return "paper wins";
-        }
-    }
-    if (choice1 === "paper") {
-        if (choice2 === "rock") {
-            return "paper wins";
-        }
-        else {
-            return "scissors wins";
-        }
-    }
-    if (choice1 === "scissors") {
-        if (choice2 === "rock") {
-            return "rock wins";
-        }
-        else {
-            return "scissors wins";
-        }
-    }
-};
-compare(userChoice, computerChoice);
+}
