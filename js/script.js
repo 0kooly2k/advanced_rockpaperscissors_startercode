229f85165b25fbd5fe452ad4fe0bfe7acb377c67
lucas rodriguez <lucasrodriguez805@gmail.com>
2017-12-20 23:28:15 +0000

    complete

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-    computerChoice = "rock";
-}
-else if (computerChoice <= 0.67) {
-    computerChoice = "paper";
-}
-else {
-    computerChoice = "scissors";
-}
-
 $(document).ready(function() {
+    $(".gif").hide();
     console.log("ready!");
     $("#shoot").click(function() {
         var userChoice = $("#choice").val()
@@ -28,20 +17,35 @@ function getComputerChoice() {
     var index = Math.floor(Math.random() * 3);
     return options[index];
 }
+
 function chooseWinner(userChoice, computerChoice) {
-    if(userChoice==computerChoice){
+    $(".gif").hide();
+    if (userChoice == computerChoice) {
         console.log("It's A Tie")
-    } else if (userChoice=="rock"&&computerChoice=="paper"){
+        $("#tie").show();
+    }
+    else if (userChoice == "rock" && computerChoice == "paper") {
         console.log("Computer Wins!")
-    } else if (userChoice=="rock"&&computerChoice=="scissors"){
+        $("#loser").show();
+    }
+    else if (userChoice == "rock" && computerChoice == "scissors") {
         console.log("User Wins!")
-    } else if (userChoice=="paper"&&computerChoice=="scissors"){
+        $("#winner").show();
+    }
+    else if (userChoice == "paper" && computerChoice == "scissors") {
         console.log("Computer Wins!")
-    } else if (userChoice=="paper"&&computerChoice=="rock"){
+        $("#loser").show();
+    }
+    else if (userChoice == "paper" && computerChoice == "rock") {
         console.log("User Wins!")
-    } else if (userChoice=="scissors"&&computerChoice=="paper"){
+        $("#winner").show();
+    }
+    else if (userChoice == "scissors" && computerChoice == "paper") {
         console.log("User Wins!")
-    } else if (userChoice=="scissors"&&computerChoice=="rock"){
+        $("#winner").show();
+    }
+    else if (userChoice == "scissors" && computerChoice == "rock") {
         console.log("Computer Wins!")
+        $("#loser").show();
     }
 }
